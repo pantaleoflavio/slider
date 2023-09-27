7e9be121fce3b4455a3560bdb45a1bb0ce1ffe2c
flavio <flavio.pantaleo@yahoo.com>
2023-09-27 20:27:53 +0200

    add prev and next buttons functionalities

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,6 +5,25 @@ const Slider = () => {
   const [recensioni, setRecensioni] = useState(data);
   const [active, setActive] = useState(0);
 
+  // go to next slide
+  const nextSlide = () => {
+    setActive((prevValue) => {
+      if (prevValue + 1 > recensioni.length - 1) {
+        return 0
+      }
+      return prevValue + 1
+    })
+  }
+
+  // go to prev slide
+  const prevSlide = () => {
+    setActive((prevValue) => {
+      if (prevValue - 1 < 0) {
+        return recensioni.length - 1
+      } 
+      return prevValue - 1
+    })
+  }
   return (<div className="container slider">
     {recensioni.map( (recensione, index) => {
       let positionClass = "";
@@ -18,10 +37,10 @@ const Slider = () => {
       return <Slide key={recensione.id} {...recensione} classes={positionClass} />
     })}
     <div className="btn-group slider-btn-group">
-      <button className="btn btn-slider prev-slider">
+      <button className="btn btn-slider prev-slider" onClick={prevSlide}>
         prev
       </button>
-      <button className="btn btn-slider next-slider">
+      <button className="btn btn-slider next-slider" onClick={nextSlide}>
         next
       </button>
     </div>
